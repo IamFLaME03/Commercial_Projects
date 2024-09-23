2ff156c005a89877e7f042a23bab2a3d28390297
Kirtan Patel <134549574+IamFLaME03@users.noreply.github.com>
2024-09-24 01:19:21 +0530

    layout ready

---
+++ foodStoreMenu/script.js
@@ -0,0 +1,17 @@
+const dropdown = document.querySelectorAll(".dropdown-btn");
+
+dropdown.forEach((i) => {
+    i.addEventListener("click", function (e) {
+        this.classList.toggle("active");
+        const dropdownContent = this.nextElementSibling;
+        let text  = e.target.id;
+        
+        if(dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+            i.innerHTML = `${text} <i class="fa-solid fa-chevron-down"></i>`
+        } else{
+            dropdownContent.style.display = "block";
+            i.innerHTML = `${text} <i class="fa-solid fa-chevron-up"></i>`
+        }
+    });
+})
